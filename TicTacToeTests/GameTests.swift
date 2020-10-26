daac812fe6526399dd1342e2428f8effd26e93ef
Marco Mustapic <marco.mustapic@persgroep.net>
2020-10-26 23:05:51 +0100

    some Game tests

--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -10,4 +10,43 @@ import XCTest
 
 class GameTests: XCTestCase {
 
+    func testGameStart() {
+        let game = Game()
+        let expectedEvents: [TestGameDelegateEvent] = [
+            .upated(.empty, .top, .left), .upated(.empty, .top, .center), .upated(.empty, .top, .right),
+            .upated(.empty, .middle, .left), .upated(.empty, .middle, .center), .upated(.empty, .middle, .right),
+            .upated(.empty, .bottom, .left), .upated(.empty, .bottom, .center), .upated(.empty, .bottom, .right),
+            .turnChanged(.cross)
+        ]
+        let delegate = TestGameDelegate(expectation: expectation(description: "board is initialised and first turn is set to cross"), expectedEventCount: expectedEvents.count)
+        game.delegate = delegate
+        game.start()
+        waitForExpectations(timeout: 0.05) { (error) in
+            if let _ = error {
+                XCTFail("Delegate not called")
+            }
+            XCTAssertEqual(expectedEvents, delegate.events)
+        }
+    }
+    
+    func testGamePlay() {
+        let game = Game()
+        let expectedEvents: [TestGameDelegateEvent] = [
+            .upated(.cross, .top, .left),
+            .turnChanged(.circle),
+            .upated(.circle, .top, .center),
+            .turnChanged(.cross)
+        ]
+        let delegate = TestGameDelegate(expectation: expectation(description: "board is initialised and first turn is set to cross"), expectedEventCount: expectedEvents.count)
+        game.start()
+        game.delegate = delegate
+        game.play(row: .top, column: .left)
+        game.play(row: .top, column: .center)
+        waitForExpectations(timeout: 0.05) { (error) in
+            if let _ = error {
+                XCTFail("Delegate not called")
+            }
+            XCTAssertEqual(expectedEvents, delegate.events)
+        }
+    }
 }
---
+++ TicTacToeTests/TestGameDelegate.swift
@@ -0,0 +1,60 @@
+//
+//  TestGameDelegate.swift
+//  TicTacToeTests
+//
+//  Created by Marco Mustapic on 26/10/2020.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class TestGameDelegate: GameDelegate {
+    
+    fileprivate let expectation: XCTestExpectation
+    fileprivate let expectedEventCount: Int
+    var events: [TestGameDelegateEvent] = []
+
+    init(expectation: XCTestExpectation, expectedEventCount: Int) {
+        self.expectation = expectation
+        self.expectedEventCount = expectedEventCount
+    }
+
+    func game(_ game: Game, updatedValue value: BoardValue, at row: BoardRow, _ column: BoardColumn) {
+        eventArrived(event: .upated(value, row, column))
+    }
+
+    func game(_ game: Game, finishedWithCondition condition: GameFinishCondition) {
+        eventArrived(event: .finished(condition))
+    }
+
+    func game(_ game: Game, turnChangedTo player: Player) {
+        eventArrived(event: .turnChanged(player))
+    }
+
+    fileprivate func eventArrived(event: TestGameDelegateEvent) {
+        events.append(event)
+        if events.count == expectedEventCount {
+            expectation.fulfill()
+        }
+    }
+}
+
+
+enum TestGameDelegateEvent: Equatable {
+    case upated(BoardValue, BoardRow, BoardColumn)
+    case finished(GameFinishCondition)
+    case turnChanged(Player)
+}
+
+extension GameFinishCondition: Equatable {
+    public static func == (lhs: GameFinishCondition, rhs: GameFinishCondition) -> Bool {
+        switch (lhs, rhs) {
+        case (.won(let lplayer), .won(let rplayer)):
+            return lplayer == rplayer
+        case (.tie, .tie):
+            return true
+        default:
+            return false
+        }
+    }
+}
