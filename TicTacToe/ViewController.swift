5001f473d58197aabc0a0c30b4a34b4fe9777efb
Marco Mustapic <marco.mustapic@persgroep.net>
2020-10-28 00:02:23 +0100

    board in viewcontroller

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -13,30 +13,92 @@ class ViewController: UIViewController {
     
     private let currentPlayerLabel: UILabel = UILabel()
     private let startButton: UIButton = UIButton(type: .custom)
+    private var buttons: Dictionary<Position, UIButton> = Dictionary()
+    private let board: UIView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.delegate = self
         
-        layoutViews()
+        createViews()
         connectViews()
     }
     
-    private func layoutViews() {
-        currentPlayerLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(currentPlayerLabel)
-        currentPlayerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20.0).isActive = true
-        currentPlayerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    private func createViews() {
         
-        startButton.translatesAutoresizingMaskIntoConstraints = false
+        createCurrentPlayerLabel()
+        createBoard()
+        createStartButton()
+
+        createContainer()
+    }
+
+    private func createCurrentPlayerLabel() {
+        currentPlayerLabel.text = " "
+    }
+    
+    private func createStartButton() {
         startButton.setTitleColor(.black, for: .normal)
         startButton.backgroundColor = .init(white: 0.9, alpha: 1.0)
-        view.addSubview(startButton)
-        startButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10.0).isActive = true
-        startButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10.0).isActive = true
-        startButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         startButton.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
         startButton.setTitle("Start", for: .normal)
+    }
+    
+    private func createBoard() {
+        
+        let positions: [[Position]] = [
+            [.topLeft, .topCenter, .topRight],
+            [.middleLeft, .middleCenter, .middleRight],
+            [.bottomLeft, .bottomCenter, .bottomRight]
+        ]
+        
+        // create all the buttons and put them in the dictionary
+        for position in positions.flatMap({$0}) {
+            buttons[position] = createBoardButton()
+        }
+        
+        // now create board
+        let rows = positions.map { (positions) -> UIView in
+            let views = positions.compactMap { (position) -> UIButton? in
+                return buttons[position]
+            }
+            let buttonsStackView = UIStackView(arrangedSubviews: views)
+            buttonsStackView.axis = .horizontal
+            buttonsStackView.alignment = .fill
+            return buttonsStackView
+        }
+        let rowsStackView = UIStackView(arrangedSubviews: rows)
+        rowsStackView.axis = .vertical
+        rowsStackView.alignment = .fill
+        rowsStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        board.addSubview(rowsStackView)
+        rowsStackView.leftAnchor.constraint(equalTo: board.leftAnchor).isActive = true
+        rowsStackView.rightAnchor.constraint(equalTo: board.rightAnchor).isActive = true
+        rowsStackView.topAnchor.constraint(equalTo: board.topAnchor).isActive = true
+        rowsStackView.bottomAnchor.constraint(equalTo: board.bottomAnchor).isActive = true
+    }
+    
+    private func createBoardButton() -> UIButton {
+        let button = UIButton(type: .custom)
+        button.layer.borderWidth = 1.0
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1.0).isActive = true
+        return button
+    }
+    
+    private func createContainer() {
+        let container = UIStackView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.axis = .vertical
+        container.spacing = 10.0
+        container.addArrangedSubview(currentPlayerLabel)
+        container.addArrangedSubview(board)
+        container.addArrangedSubview(startButton)
+        view.addSubview(container)
+        container.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10.0).isActive = true
+        container.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10.0).isActive = true
+        container.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0.0).isActive = true
     }
     
     private func connectViews() {
